b554ae5c19bff68a983d65c0f098a3a61b731e80
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-08-20 20:49:22 +0200

    Added fillables - Test seeder done

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
+use App\Models\Project;
 class ProjectSeeder extends Seeder
 {
     /**
@@ -12,8 +13,15 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=1; $i<11; $i++){
+            $project = new Project();
+
+            $project->title = $faker->sentence(3);
+            $project->description = $faker->text(500);
+            $project->used_apps = $faker->words( 2, true);
+            $project->save();
+        }
     }
 }
