93fb322c3f05ac3f28ccf7a04348b1a6eef5078c
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-08-22 00:03:52 +0200

    Modified index view layout

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -6,42 +6,38 @@
             <div class="col-12 mt-5">
                 <h1>I miei progetti</h1>
                 <div class="col-12 mt-5">
-                    <table class="table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Titolo</th>
-                                <th>Descrizione</th>
-                                <th>App utilizzate</th>
-                                <th>Azioni</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($projects as $project)
-                                <tr>
-                                    <td>{{$project->id}}</td>
-                                    <td>{{$project->title}}</td>
-                                    <td>{{$project->description}}</td>
-                                    <td>{{$project->used_apps}}</td>
-                                    <td>
-                                        <a href="#" class="btn btn-sm btn-primary">
-                                            <i class="fas fa-eye"></i>
-                                        </a>
-                                        <a href="#" class="btn btn-sm btn-warning">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                        <form class="d-line-block" action="#" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-sm btn-danger">
-                                                <i class="fas fa-trash"></i>
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    @foreach ($projects as $project)
+                    <div class="card my-4">
+                        <!-- CARD HEADER -->
+                        <h5 class="card-header d-flex justify-content-between">
+                            <div>
+                                {{$project->id}}
+                            </div>
+                            <div class="d-flex">
+                                <a href="#" class="btn btn-sm btn-primary mx-2">
+                                <i class="fas fa-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-sm btn-warning mx-2">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <form class="d-line-block" action="#" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-sm btn-danger mx-2">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </h5>
+
+                        <!-- CARD BODY -->
+                        <div class="card-body">
+                            <h5 class="card-title">{{$project->title}}</h5>
+                            <p class="card-text">{{$project->description}}</p>
+                            <p class="card-text">App utilizzate: {{$project->used_apps}}</p>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
