9aa0c16cc4b78128e42f468320378ab2d2d1d518
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-08-24 09:21:01 +0200

    [FEAT] added file uploader on create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -15,21 +15,28 @@
                     <label class="control-label">Titolo: </label>
                     <input type="text" id="title" class="form-control" placeholder="Titolo del progetto" name="title" value="{{old('title')}}">
                     @error('title')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="form-group mb-3">
+                    <label class="control-label">Immagine:</label>
+                    <input type="file" name="img" id="img" class="form-control" >
+                    @error('img')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group mb-3">
                     <label class="control-label">Descrizione:</label>
                     <input type="text" id="descrizione" class="form-control" placeholder="Descrizione" name="descrizione" value="{{old('descrizione')}}">
                     @error('descrizione')
-                            <div class="text-danger">{{ $message }}</div>
+                        <div class="text-danger">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group mb-3">
                     <label class="control-label">Applicazioni utilizzate:</label>
                     <input type="text" id="used_apps" class="form-control" placeholder="Inerisci le app separati da una virgola" name="used_apps" value="{{old('used_apps')}}">
                     @error('used_apps')
-                            <div class="text-danger">{{ $message }}</div>
+                        <div class="text-danger">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="col-12 d-flex justify-content-center align-items-center">
